242c745fe1996763503438cd1c0f580418b359cb
Franco Aparicio <ffrancoaparicio@gmail.com>
2024-06-05 21:52:00 -0300

    first steps

---
+++ src/components/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className='main-container'>
+      <article className='movie-card'>
+        <img
+          className='img-background'
+          src='https://images.hdqwalls.com/wallpapers/bthumb/avatar-the-way-of-the-water-2022-5k-u1.jpg'
+          alt='Avatar wallpaper'
+        />
+
+        <div className='content'>
+          <h2 className='title'>Avatar: The Way of Water</h2>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default Main;
